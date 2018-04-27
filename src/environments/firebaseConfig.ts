8492c5be29b15ee8abf2ab71a77d13910f81409d
Christopher Woods <cjwoods89@gmail.com>
2018-04-27 16:31:00 -0400

    Initial commit by ngx-firebase-bootstrap generator

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,11 @@
+
+
+export const firebaseConfig = {
+    // Paste all this from the Firebase console...
+    apiKey: "AIzaSyCQim8HPTcB9pMjuPq7z7tXzHX0mLQlcSI",
+    authDomain: "rmxew-lmfa.firebaseapp.com",
+    databaseURL: "https://rmxew-lmfa.firebaseio.com",
+    storageBucket: "rmxew-lmfa.appspot.com",
+    messagingSenderId: "1073259399972"
+};
+
